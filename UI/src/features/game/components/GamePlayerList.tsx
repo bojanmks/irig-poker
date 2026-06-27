04173ede5184b17bb626519ed27127693d532208
Bojan Maksimovic <bojanm245@gmail.com>
2026-06-27 20:55:31 +0200

    Make GamePlayerList always display the player whose turn it is first

--- UI/src/features/game/components/GamePlayerList.tsx
+++ UI/src/features/game/components/GamePlayerList.tsx
@@ -9,7 +9,18 @@ export const GamePlayerList = () => {
     const playerId = useAppSelector((state) => state.gameState.playerId);
 
     const players = useMemo(() => {
-        return gameState!.playerOrder.map(id => gameState!.players[id]);
+        if (!gameState) {
+            return [];
+        }
+
+        const { playerOrder, currentTurnPlayerId } = gameState;
+        const turnIndex = currentTurnPlayerId ? playerOrder.indexOf(currentTurnPlayerId) : -1;
+
+        const order = turnIndex > -1
+            ? [...playerOrder.slice(turnIndex), ...playerOrder.slice(0, turnIndex)]
+            : playerOrder;
+            
+        return order.map(id => gameState.players[id]);
     }, [gameState]);
 
     return (
