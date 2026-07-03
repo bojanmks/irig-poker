dc95ecfa8b8d54fe8c9fd630a1db4d404a2b401e
Bojan Maksimovic <bojanm245@gmail.com>
2026-07-03 22:11:22 +0200

    Add AllowForRoles attribute where it was missing

--- WebApi.Application/Features/Games/Commands/ClaimHandCommand.cs
+++ WebApi.Application/Features/Games/Commands/ClaimHandCommand.cs
@@ -1,7 +1,10 @@
 using MediatR;
+using WebApi.Application.Core.Cqrs;
+using WebApi.Common.Core.Auth.Enums;
 using WebApi.Common.Core.Result.Models;
 using WebApi.Common.Features.Games.Models;
 
 namespace WebApi.Application.Features.Games.Commands;
 
+[AllowForRoles(UserRole.RoomOwner, UserRole.Player)]
 public record ClaimHandCommand(ClaimHandRequest Data) : IRequest<Result<ClaimResult>>;
