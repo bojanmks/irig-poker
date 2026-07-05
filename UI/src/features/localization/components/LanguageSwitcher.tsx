f9b42c80f4b762b6f450674a8589594bd4029d39
Bojan Maksimovic <bojanm245@gmail.com>
2026-07-05 16:24:03 +0200

    Update lang replacement logic in LanguageSwitcher

--- UI/src/features/localization/components/LanguageSwitcher.tsx
+++ UI/src/features/localization/components/LanguageSwitcher.tsx
@@ -1,6 +1,6 @@
 import { useCallback,useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import {
   Select,
