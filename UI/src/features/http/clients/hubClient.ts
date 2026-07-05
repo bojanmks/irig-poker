8176de71d8213baf1654a4e21e60c788aca7e04f
Bojan Maksimovic <bojanm245@gmail.com>
2026-07-05 17:18:54 +0200

    Disable FE SignalR loglevel in non dev envs

--- UI/src/features/http/clients/hubClient.ts
+++ UI/src/features/http/clients/hubClient.ts
@@ -1,8 +1,12 @@
 import * as signalR from "@microsoft/signalr";
 
+const logLevel = import.meta.env.DEV
+  ? signalR.LogLevel.Information
+  : signalR.LogLevel.None;
+
 const connection = new signalR.HubConnectionBuilder()
   .withUrl(`${import.meta.env.VITE_API_BASE_URL}/hubs/game`)
-  .configureLogging(signalR.LogLevel.Information)
+  .configureLogging(logLevel)
   .build();
 
 export default connection;
