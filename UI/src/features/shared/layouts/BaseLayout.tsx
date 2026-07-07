7461c81e04fadda6da59bacd49e4f19ce50feb0b
Bojan Maksimovic <bojanm245@gmail.com>
2026-07-07 19:53:09 +0200

    Add logo

--- UI/src/features/shared/layouts/BaseLayout.tsx
+++ UI/src/features/shared/layouts/BaseLayout.tsx
@@ -1,15 +1,22 @@
 // BaseLayout.tsx
 import { useMemo } from "react"
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 import clsx from "clsx"
 import { GitBranch } from "lucide-react"
 
+import logo from "@/assets/logo.png"
 import MuteToggle from "@/features/audio/components/MuteToggle"
 import LanguageSwitcher from "@/features/localization/components/LanguageSwitcher"
 import { buttonVariants } from "@/features/shared/components/shadcn/Button"
 import { useAppSelector } from "@/features/store/hooks"
 import ThemeSwitcher from "@/features/themes/components/ThemeSwitcher"
+
+const Logo = () => (
+  <Link to="/" className="flex items-center">
+    <img src={logo} alt="Logo" className="h-10 lg:h-14 w-auto" />
+  </Link>
+)
 
 const BaseLayout = () => {
   const additionalClass = useAppSelector((state) => state.wrapperClass.additionalClass)
@@ -19,18 +26,21 @@ const BaseLayout = () => {
 
   return (
     <div>
-      <div className="py-4 pr-4 flex justify-end items-center gap-2">
-        <a 
-          href="https://github.com/bojanmks/irig-poker" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className={buttonVariants({ variant: "outline", size: "icon" })}
-        >
-          <GitBranch size={16} />
-        </a>
-        <MuteToggle />
-        <ThemeSwitcher />
-        <LanguageSwitcher />
+      <div className="py-4 px-4 flex justify-between items-center gap-2">
+        <Logo />
+        <div className="flex items-center gap-2">
+          <a 
+            href="https://github.com/bojanmks/irig-poker" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className={buttonVariants({ variant: "outline", size: "icon" })}
+          >
+            <GitBranch size={16} />
+          </a>
+          <MuteToggle />
+          <ThemeSwitcher />
+          <LanguageSwitcher />
+        </div>
       </div>
 
       <div className="p-2">
