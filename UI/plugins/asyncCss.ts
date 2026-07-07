35052945bf271e2adcf41447ac619fd8a7155469
Bojan Maksimovic <bojanm245@gmail.com>
2026-07-07 20:26:19 +0200

    Fix unused variable error

--- UI/plugins/asyncCss.ts
+++ UI/plugins/asyncCss.ts
@@ -6,7 +6,7 @@ export function asyncCssPlugin(): Plugin {
     transformIndexHtml(html) {
       return html.replace(
         /<link\s+rel="stylesheet"\s+([^>]*)\/?>/g,
-        (match, attrs) =>
+        (_, attrs) =>
           `<link rel="preload" as="style" ${attrs} onload="this.onload=null;this.rel='stylesheet'"><noscript><link rel="stylesheet" ${attrs}></noscript>`
       );
     },
